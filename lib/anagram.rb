36c3c7ba7793dd440cd88eb9a8ab422dedc31aec
louis baptiste <louisvis@ymail.com>
2018-06-26 07:40:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,31 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+attr_accessor :word
+@anagram_sorted = []
+@word_match=[]
+
+
+def initialize (word)
+@word = word
+@word_anagram =word.split("")
+@anagram_sorted = @word_anagram.sort
+end
+
+def match (strings)
+    stringlist= strings
+    stringlist.each do |word|
+    letter_array = word.split("")
+    letter_sorted =letter_array.sort
+    if @anagram_sorted ==letter_sorted
+      return word
+    else
+      return @word_match
+    end
+
+    end
+
+end
+
+end
